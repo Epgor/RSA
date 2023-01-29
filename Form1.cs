8e147b41fe18b5f54136e03388ce3d006629e6af
Epgor <61190141+Epgor@users.noreply.github.com>
2023-01-29 15:37:46 +0100

    final commit v1.0

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,44 @@ namespace RSA
 {
     public partial class Form1 : Form
     {
+        private RSAEncryption cipher; 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var inputText = textBox1.Text;
+
+            var _e = (int)numericUpDown1.Value;
+            var _d = (int)numericUpDown2.Value;
+            var _n = (int)numericUpDown3.Value;
+
+            cipher = new RSAEncryption(inputText, _e, _d, _n);
+            cipher.Encrypt();
+
+            textBox2.Text = cipher.GetCipherText();
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            cipher.SetCipherText(textBox2.Text);
+            cipher.SetEDN(
+                (int)numericUpDown1.Value,
+                (int)numericUpDown2.Value,
+                (int)numericUpDown3.Value);
+            
+            cipher.Decrypt();
+
+            textBox3.Text = cipher.GetPlainText();
         }
     }
 }
---
+++ RSAEncryption.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+
+namespace RSA
+{
+    internal class RSAEncryption
+    {
+        private string _plainText, _cipherText;
+        private int e, d, n;
+
+        public RSAEncryption(string plainText, int e, int d, int n)
+        {
+            _plainText = plainText;
+            this.e = e;
+            this.d = d;
+            this.n = n;
+        }
+
+        public string GetPlainText() { return _plainText; }
+        public string GetCipherText() { return _cipherText; }
+        public void SetCipherText(string cipher) { _cipherText = cipher; }
+
+        public void SetEDN (int e, int d, int n)
+        {
+            this.e = e;
+            this.d = d;
+            this.n = n;
+        }
+
+        public void Encrypt()
+        {
+            int eValue = e;
+            int dValue = d;
+            int nValue = n;
+
+            string plainText = _plainText;
+            BigInteger[] encryptedMessage = new BigInteger[plainText.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                BigInteger character = (int)plainText[i];
+                encryptedMessage[i] = BigInteger.ModPow(character, eValue, nValue);
+            }
+
+            string encryptedMessageString = string.Join(",", encryptedMessage);
+            _cipherText = encryptedMessageString;
+        }
+
+        public void Decrypt()
+        {
+            int dValue = d;
+            int nValue = n;
+
+            string[] encryptedMessageString = _cipherText.Split(',');
+            BigInteger[] encryptedMessage = new BigInteger[encryptedMessageString.Length];
+
+            for (int i = 0; i < encryptedMessageString.Length; i++)
+            {
+                encryptedMessage[i] = BigInteger.Parse(encryptedMessageString[i]);
+            }
+
+            char[] decryptedMessage = new char[encryptedMessage.Length];
+
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                BigInteger character = BigInteger.ModPow(encryptedMessage[i], dValue, nValue);
+                decryptedMessage[i] = (char)character;
+            }
+
+            string plainText = new string(decryptedMessage);
+            _plainText = plainText;
+        }
+    }
+}
+
